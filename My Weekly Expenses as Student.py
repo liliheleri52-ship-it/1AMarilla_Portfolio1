e7a273b76678695743b4fa9149737daaaead6263
Ashley Marilla <liliheleri52@gmail.com>
2026-05-12 21:18:59 +0800

    Add files via upload

---
+++ My Weekly Expenses as Student.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[45]:
+
+
+print('Ashley M. Marilla')
+print('Bicol University')
+Days_per_week = 5
+Daily_allowance = 500
+print("Days per week:", Days_per_week)
+print("Daily allowance: ₱", Daily_allowance)
+
+
+# In[7]:
+
+
+transportation = 80
+food = 120
+supplies = 50
+# Updted value
+transport = 2100
+print("transportation: ₱", transportation)
+print("food: ₱", food)
+print("supplies: ₱", supplies)
+
+
+# In[13]:
+
+
+daily_expense = 90 + 120 + 50
+weekly_expense = daily_expenses*5
+print("My Daily expense: ₱, daily_expense)
+print("My Weekly Expense: ₱, weekly_expense)
+
+
+# In[21]:
+
+
+print("Allowance - Expense: ₱", 500 - 260) #savings
+print("Expense x 4 weeks: ₱", 260*4)
+print("Half of Allowance: ₱", 500/2)
+print("Remainder of 500/3:", 500 % 3)
+
+
+# In[28]:
+
+
+print(type(Daily_allowance)) 
+print(type("Bicol University"))
+text_amount = "350"
+num_amount = int(text_amount)
+print("Converted amount: ₱", num_amount)
+print(type(num_amount))
+
+
+# In[31]:
+
+
+my_transportation = int(input("Transport: ₱"))
+my_food = int(input("Food: ₱"))
+my_total = my_transportation + my_food
+print("Your daily total: ₱", my_total)
+
+
+# In[44]:
+
+
+Student = "Ashley M. Marilla"
+School = "Bicol University"
+Weekly_allowance = 500
+Days = 5
+transportation = 80
+food = 120
+other = 50
+daily = transportation + food + other
+weekly = daily*Days
+savings = Weekly_allowance - daily
+print("-"*35)
+print("Student: Ashley M. Marilla")
+print(School)
+print("-"*35)
+print("Daily Expenses: ₱", daily)
+print("Weekly Expense: ₱", weekly)
+print("Daily Savings: ₱", savings)
+print("-"*35)
+
+
+
+# In[ ]:
+
+
+
+
